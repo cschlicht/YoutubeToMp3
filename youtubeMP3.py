1dadd171f30cdbe2f5f95869570102369236d293
Cameron Schlicht <cameronschlicht7@gmail.com>
2023-04-06 12:34:41 -0400

    removing .mp4 file appearing in code directory

--- youtubeMP3.py
+++ youtubeMP3.py
@@ -13,27 +13,25 @@ def download_mp3():
     url = url_entry.get()
     url = url.split("&")[0]
     video = YouTube(url)
-    audio_stream = video.streams.filter(only_audio=True).first()
+    audio_stream = video.streams.filter(mime_type="audio/mp4").first()
     audio_file = audio_stream.download()
     video_title = video.title.replace("|","-").replace(":","-").replace("\"","'")
     mp3_file = f"{path_entry.get()}/{video_title}.mp3"
     AudioFileClip(audio_file).write_audiofile(mp3_file)
+    os.remove(audio_file)  # remove the downloaded .mp4 file
     info_label.config(text="Download complete!")
 
 root = Tk()
 root.title("YouTube MP3 Downloader")
 
-directory_frame = Frame(root)
-directory_frame.pack()
-
-directory_label = Label(directory_frame, text="Choose download directory:")
-directory_label.pack(side=LEFT)
-
-directory_button = Button(directory_frame, text="Choose", command=choose_directory)
-directory_button.pack(side=LEFT)
+directory_label = Label(root, text="Choose download directory:")
+directory_label.pack()
 
 path_entry = Entry(root, width=50)
 path_entry.pack()
+
+directory_button = Button(root, text="Choose", command=choose_directory)
+directory_button.pack()
 
 url_label = Label(root, text="Paste YouTube URL below:")
 url_label.pack()
