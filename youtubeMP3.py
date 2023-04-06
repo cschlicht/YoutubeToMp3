0bbd6b18885caf2076fe32f0044eab5af13435e7
Cameron Schlicht <cameronschlicht7@gmail.com>
2023-04-06 13:49:55 -0400

    upgrading GUI

--- youtubeMP3.py
+++ youtubeMP3.py
@@ -1,89 +1,122 @@
-from tkinter import *
-from tkinter import filedialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QFrame, QHBoxLayout, QVBoxLayout, QPushButton, QLineEdit, QFileDialog
 from pytube import YouTube
 from moviepy.editor import *
 import pyperclip
 import os
 import configparser
+import sys
 
-# Create a config parser object
-config = configparser.ConfigParser()
+class YouTubeDownloader(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        # Create a config parser object
+        self.config = configparser.ConfigParser()
 
-# Check if configuration file exists
-if os.path.exists('config.ini'):
-    # If it exists, read the path from the config file
-    config.read('config.ini')
-    path = config.get('Settings', 'path')
-else:
-    # If it doesn't exist, set path to an empty string
-    path = ''
+        # Check if configuration file exists
+        if os.path.exists('config.ini'):
+            # If it exists, read the path from the config file
+            self.config.read('config.ini')
+            self.path = self.config.get('Settings', 'path')
+        else:
+            # If it doesn't exist, set path to an empty string
+            self.path = ''
+        
+        self.initUI()
+        
+    def initUI(self):
+        # create main window
+        self.setWindowTitle("YouTube MP3 Downloader")
+        self.setGeometry(100, 100, 575, 300)
 
-def choose_directory():
-    global path
-    path = filedialog.askdirectory()
-    directory_label.config(text="Download Directory: ")
-    path_entry.delete(0, END)
-    path_entry.insert(0, path)
-    # Save the path to the configuration file
-    config['Settings'] = {'path': path}
-    with open('config.ini', 'w') as configfile:
-        config.write(configfile)
+        # create download directory frame
+        directory_frame = QFrame(self)
+        directory_frame.setFrameShape(QFrame.StyledPanel)
+        directory_frame.setFrameShadow(QFrame.Raised)
 
-def paste_clipboard():
-    clipboard_data = pyperclip.paste()
-    url_entry.delete(0, END)
-    url_entry.insert(0, clipboard_data)
+        directory_layout = QHBoxLayout()
+        directory_frame.setLayout(directory_layout)
 
-def download_mp3():
-    url = url_entry.get()
-    url = url.split("&")[0]
-    video = YouTube(url)
-    audio_stream = video.streams.filter(mime_type="audio/mp4").first()
-    audio_file = audio_stream.download()
-    video_title = video.title.replace("|","-").replace(":","-").replace("\"","'")
-    mp3_file = f"{path}/{video_title}.mp3"
-    AudioFileClip(audio_file).write_audiofile(mp3_file)
-    os.remove(audio_file)  # remove the downloaded .mp4 file
-    info_label.config(text="Download complete!")
+        directory_label = QLabel("Download Directory:", self)
+        directory_layout.addWidget(directory_label)
 
-# create main window
-root = Tk()
-root.title("YouTube MP3 Downloader")
-root.geometry('575x300')
+        self.path_entry = QLineEdit(self)
+        self.path_entry.setText(self.path)
+        self.path_entry.setReadOnly(True)
+        directory_layout.addWidget(self.path_entry)
 
-# create download directory frame
-directory_frame = Frame(root, bd=2, relief="groove")
-directory_frame.pack(side=TOP, fill=X, padx=10, pady=10)
+        directory_button = QPushButton("Choose", self)
+        directory_button.clicked.connect(self.choose_directory)
+        directory_layout.addWidget(directory_button)
 
-directory_label = Label(directory_frame, text="Download Directory: ")
-directory_label.pack(side=LEFT, padx=10, pady=10)
+        # create URL frame
+        url_frame = QFrame(self)
+        url_frame.setFrameShape(QFrame.StyledPanel)
+        url_frame.setFrameShadow(QFrame.Raised)
 
-path_entry = Entry(directory_frame, width=50)
-path_entry.pack(side=LEFT, padx=10, pady=10)
-path_entry.insert(0, path)
+        url_layout = QHBoxLayout()
+        url_frame.setLayout(url_layout)
 
-directory_button = Button(directory_frame, text="Choose", command=choose_directory, padx=10, pady=10)
-directory_button.pack(side=LEFT, padx=10, pady=10)
+        url_label = QLabel("YouTube URL:", self)
+        url_layout.addWidget(url_label)
 
-# create URL frame
-url_frame = Frame(root, bd=2, relief="groove")
-url_frame.pack(side=TOP, fill=X, padx=10, pady=10)
+        self.url_entry = QLineEdit(self)
+        url_layout.addWidget(self.url_entry)
 
-url_label = Label(url_frame, text="YouTube URL: ")
-url_label.pack(side=LEFT, padx=10, pady=10)
+        paste_button = QPushButton("Paste", self)
+        paste_button.clicked.connect(self.paste_clipboard)
+        url_layout.addWidget(paste_button)
 
-url_entry = Entry(url_frame, width=50)
-url_entry.pack(side=LEFT, padx=10, pady=10, anchor='center')
+        # create download button
+        download_button = QPushButton("Download MP3", self)
+        download_button.clicked.connect(self.download_mp3)
 
-paste_button = Button(url_frame, text="Paste", command=paste_clipboard, padx=10, pady=10)
-paste_button.pack(side=LEFT, padx=10, pady=10)
+        # create info label
+        self.info_label = QLabel(self)
 
-# create download button
-download_button = Button(root, text="Download MP3", command=download_mp3, padx=10, pady=10)
-download_button.pack(side=TOP, padx=10, pady=10)
+        # create layout for main window
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(directory_frame)
+        main_layout.addWidget(url_frame)
+        main_layout.addWidget(download_button)
+        main_layout.addWidget(self.info_label)
 
-# create info label
-info_label = Label(root, text="", padx=10, pady=10)
-info_label.pack(side=BOTTOM)
+        # set the layout of the main window
+        main_widget = QFrame(self)
+        main_widget.setLayout(main_layout)
+        self.setCentralWidget(main_widget)
 
-root.mainloop()
+    def choose_directory(self):
+        path = QFileDialog.getExistingDirectory(self, "Choose Directory")
+        if path:
+            self.path = path
+            self.path_entry.setText(self.path)
+            # Save the path to the configuration file
+            self.config['Settings'] = {'path': self.path}
+            with open('config.ini', 'w') as configfile:
+                self.config.write(configfile)
+
+    def paste_clipboard(self):
+        clipboard_data = pyperclip.paste()
+        self.url_entry.setText(clipboard_data)
+
+    def download_mp3(self):
+        url = self.url_entry.text()
+        url = url.split("&")[0]
+        video = YouTube(url)
+        audio_stream = video.streams.filter(mime_type="audio/mp4").first()
+        audio_file = audio_stream.download()
+        audio_file_name = audio_file.split("\\")[-1]
+        audio_file_name = audio_file_name.replace(".mp4", ".mp3")
+        audio_file_path = os.path.join(self.path, audio_file_name)
+        video_clip = AudioFileClip(audio_file)
+        video_clip.write_audiofile(audio_file_path)
+        video_clip.close()
+        os.remove(audio_file)
+        self.info_label.setText("MP3 Downloaded Successfully")
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = YouTubeDownloader()
+    ex.show()
+    sys.exit(app.exec_())
